6d3f9bc2e6cf22f5f7be152689a4bd0f26d847ef
reachAkash <akashgupta9062@gmail.com>
2024-08-05 16:42:08 +0530

    update : navbar responsive.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -32,6 +32,7 @@ const Navbar: React.FC = () => {
 
   return (
     <>
+      {/* for desktop & big devices */}
       <div
         className={`sticky top-0 ${
           showShadow && "shadow"
@@ -84,6 +85,8 @@ const Navbar: React.FC = () => {
           <MiniSidebar className="block md:hidden" />
         </div>
       </div>
+
+      {/* for small devices */}
       <div
         className={`sticky top-0 ${
           showShadow && "shadow"
@@ -95,43 +98,9 @@ const Navbar: React.FC = () => {
               MENTOONS COMICS<span className="text-white">.</span>
             </h2>
           </Link>
-          <ul className="hidden md:flex w-full items-center justify-evenly gap-2 text-lg">
-            <li className="hover:text-red-500 uppercase cursor-pointer font-semibold text-gray-500 transition-all duration-300 ease-in-out">
-              Home
-            </li>
-            <li className="hover:text-red-500 uppercase cursor-pointer font-semibold text-gray-500 transition-all duration-300 ease-in-out">
-              About
-            </li>
-            <li className="hover:text-red-500 uppercase cursor-pointer font-semibold text-gray-500 transition-all duration-300 ease-in-out">
-              Comics
-            </li>
-            <li className="hover:text-red-500 uppercase cursor-pointer font-semibold text-gray-500 transition-all duration-300 ease-in-out">
-              Blog
-            </li>
-            <li className="hover:text-red-500 uppercase cursor-pointer font-semibold text-gray-500 transition-all duration-300 ease-in-out">
-              Contact
-            </li>
-          </ul>
         </div>
         <div className="w-[35%] lg:w-[40%] flex items-center justify-end gap-2 lg:gap-8">
-          <div className="hidden custom:flex items-center justify-between gap-3 bg-white rounded-full pl-6">
-            <Link to="/wishlist">
-              <FaBookmark className="text-lg cursor-pointer text-gray-500 hover:text-black transition-all duration-300 ease-in-out" />
-            </Link>
-            <Link to="/cart">
-              <div className="relative group">
-                <IoCart className="text-2xl cursor-pointer text-gray-500 group-hover:text-black transition-all duration-300 ease-in-out" />
-                <span className="absolute top-[-0.5rem] left-[-4px] text-white text-sm bg-primary rounded-full px-[6px]">
-                  {cartItems?.length}
-                </span>
-              </div>
-            </Link>
-            <button className="w-full text-base lg:text-lg py-3 px-6 font-medium rounded-full bg-[#333333] text-white hover:bg-primary transition-all duration-300 ease-in-out">
-              Free Comics
-            </button>
-          </div>
-          <Sidebar className="hidden md:block" />
-          <MiniSidebar className="block md:hidden" />
+          <MiniSidebar className="block" />
         </div>
       </div>
     </>
