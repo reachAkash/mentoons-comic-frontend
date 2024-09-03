3a8bb35251c7248772df595714af4514fe41b9af
reachAkash <akashgupta9062@gmail.com>
2024-09-03 16:33:36 +0530

    update : breadcrumbs updated

--- src/components/common/BreadCrumbs.tsx
+++ src/components/common/BreadCrumbs.tsx
@@ -4,7 +4,7 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 const Breadcrumbs = () => {
   const location = useLocation();
   const navigate = useNavigate();
-  const pathnames = location.pathname.split("/").filter((x) => x);
+  const pathnames = location.pathname.split("/").filter(Boolean); // Remove empty strings
 
   return (
     <nav>
@@ -12,25 +12,32 @@ const Breadcrumbs = () => {
         {pathnames.length > 0 && (
           <li>
             <button onClick={() => navigate(-1)} className="flex items-center">
-                <FaArrowLeft className="text-xl mr-3"/>
+              <FaArrowLeft className="text-xl mr-3" />
             </button>
           </li>
         )}
         <li>
           <Link to="/">
             <figure className="w-6 h-6">
-              <img src="/assets/home/Home icon .png" alt="home icon" className="h-full w-full object-contain" />
+              <img
+                src="/assets/home/Home icon .png"
+                alt="home icon"
+                className="h-full w-full object-contain"
+              />
             </figure>
           </Link>
         </li>
         {pathnames.map((value, index) => {
-          const to = `/${pathnames.slice(0, index + 1).join("/")}`;
+          // Decode the pathname to handle spaces correctly
+          const decodedPathname = decodeURIComponent(value);
+          const to = `/${pathnames.slice(0, index + 1).join("/")}`; // Template literal
 
           return (
             <li key={to}>
               <span> / </span>
               <Link to={to}>
-                {value.charAt(0).toUpperCase() + value.slice(1)}
+                {decodedPathname.charAt(0).toUpperCase() +
+                  decodedPathname.slice(1)}
               </Link>
             </li>
           );
