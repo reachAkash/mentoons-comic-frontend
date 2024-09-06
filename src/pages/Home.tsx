7f8ca75801b06f080f48d1674dfa6e650a16d5d7
Akash <undefined>
2024-09-06 15:15:18 +0530

    home page redesign mobile responsive

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,19 @@
-import ContentSection from "@/components/Home/ContentSection"
-import HeroBanner from "@/components/Home/HeroBanner"
-import MdMultiverse from "@/components/Home/MdMultiverse"
-import WhatWeOffer from "@/components/Home/WhatWeOffer"
+import ContentSection from "@/components/Home/ContentSection";
+import HeroBanner from "@/components/Home/HeroBanner";
+import MdMultiverse from "@/components/Home/MdMultiverse";
+import Struggles from "@/components/Home/Struggles";
+import WhatWeOffer from "@/components/Home/WhatWeOffer";
 
 const Home = () => {
   return (
     <div className="h-full w-full">
-      <HeroBanner/>
-      <MdMultiverse/>
-      <WhatWeOffer/>
-      <ContentSection/>
+      <HeroBanner />
+      <Struggles />
+      <MdMultiverse />
+      <WhatWeOffer />
+      <ContentSection />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
