925b66abb743a4769e96bb9e319660a5e384a3be
reachAkash <akashgupta9062@gmail.com>
2024-09-12 16:16:59 +0530

    update : podcast section in homepage

--- src/components/Home/ComicsSection.tsx
+++ src/components/Home/ComicsSection.tsx
@@ -6,14 +6,17 @@ import PortalImgHome from "@/assets/imgs/portalBgHome.svg";
 import WorkshopBgHome from "@/assets/imgs/workshopBgHome.svg";
 import ComicsBgHome from "@/assets/imgs/comicsBgHome.svg";
 import Klement_Toonla_Home from "@/assets/imgs/klement_toonlaBgHome.svg";
-// import KidsWorkshopHome from "@/assets/imgs/kidsWorkshopHome.svg";
-// import TeenWorkshopHome from "@/assets/imgs/TeenWorkshopHome.svg";
-// import ElderWorkshopHome from "@/assets/imgs/ElderWorkshopHome.svg";
-// import ComicWorkshopHome from "@/assets/imgs/comicWorkshopHome.svg";
-// import CharacterWorkshopHome from "@/assets/imgs/characterWorkshopHome.svg";
+import BuddyWorkshopHome from "@/assets/imgs/kidsWorkshopHome.svg";
+import TeenWorkshopHome from "@/assets/imgs/teenWorkshopHome.svg";
+import ElderWorkshopHome from "@/assets/imgs/elderWorkshopHome.svg";
+import ComicWorkshopHome from "@/assets/imgs/comicWorkshopHome.svg";
+import CharacterWorkshopHome from "@/assets/imgs/characterWorkshopHome.svg";
+import workshopTextHome from "@/assets/imgs/workshopTextHome.svg";
 import { easeInOut, motion } from "framer-motion";
+import { useNavigate } from "react-router-dom";
 
 const ComicsSection: React.FC = () => {
+  const navigate = useNavigate();
   return (
     <div className="space-y-10">
       <div className="relative h-full w-full">
@@ -39,10 +42,32 @@ const ComicsSection: React.FC = () => {
           />
         </div>
         <div className="absolute top-[30%] left-[10%]">
-          <img src={FlyingBook} alt="books image" />
+          <motion.img
+            initial={{ y: -20 }}
+            animate={{ y: [-20, -10, -20] }} // Smooth movement between values
+            transition={{
+              duration: 2,
+              ease: "easeInOut",
+              repeat: Infinity,
+              repeatType: "mirror", // Ensures a seamless back-and-forth motion
+            }}
+            src={FlyingBook}
+            alt="books image"
+          />
         </div>
         <div className="absolute top-[30%] right-[10%]">
-          <img src={FlyingBook} alt="books image" />
+          <motion.img
+            initial={{ y: -20 }}
+            animate={{ y: [-20, -10, -20] }} // Smooth movement between values
+            transition={{
+              duration: 2,
+              ease: "easeInOut",
+              repeat: Infinity,
+              repeatType: "mirror", // Ensures a seamless back-and-forth motion
+            }}
+            src={FlyingBook}
+            alt="books image"
+          />
         </div>
         <div className="absolute top-[60%] left-[10%]">
           <motion.img
@@ -83,9 +108,55 @@ const ComicsSection: React.FC = () => {
       </div>
       <div className="relative">
         <img src={WorkshopBgHome} alt="workshop" />
+        <div>
+          <img
+            className="absolute w-[45%] -left-[4%] top-[25%]"
+            src={workshopTextHome}
+            alt="workshop"
+          />
+        </div>
         <div className="w-full absolute rotate-180 flex items-center justify-center right-4 top-[14rem] ">
           <img className="w-1/3" src={PortalImgHome} alt="portal" />
-          <div></div>
+        </div>
+        <div className="">
+          <img
+            onClick={() => navigate("/mentoons-workshops?workshop=buddy")}
+            className="absolute right-[38%] top-[36%] w-[13%] hover:scale-105 cursor-pointer transition-all ease-in-out duration-300"
+            src={BuddyWorkshopHome}
+            alt="workshop poster"
+          />
+        </div>
+        <div>
+          <img
+            onClick={() => navigate("/mentoons-workshops?workshop=teen")}
+            className="absolute right-[10%] top-[40%] w-[15%] hover:scale-105 cursor-pointer rotate-2 transition-all ease-in-out duration-300"
+            src={TeenWorkshopHome}
+            alt="workshop poster"
+          />
+        </div>
+        <div>
+          <img
+            onClick={() => navigate("/mentoons-workshops?workshop=family")}
+            className="absolute right-[25%] top-[50%] w-[13%] hover:scale-105 cursor-pointer transition-all ease-in-out duration-300"
+            src={ElderWorkshopHome}
+            alt="workshop poster"
+          />
+        </div>
+        <div>
+          <img
+            onClick={() => navigate("/mentoons-workshops?workshop=comics")}
+            className="absolute right-[36%] top-[58%] w-[13%] hover:scale-105 cursor-pointer transition-all ease-in-out duration-300"
+            src={ComicWorkshopHome}
+            alt="workshop poster"
+          />
+        </div>
+        <div>
+          <img
+            onClick={() => navigate("/mentoons-workshops?workshop=character")}
+            className="absolute right-[14%] top-[58%] w-[13%] hover:scale-105 cursor-pointer transition-all ease-in-out duration-300"
+            src={CharacterWorkshopHome}
+            alt="workshop poster"
+          />
         </div>
       </div>
     </div>
