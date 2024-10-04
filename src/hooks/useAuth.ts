eefc2a1ed778c093debf8ef694858b3f385ee9f1
reachAkash <akashgupta9062@gmail.com>
2024-10-04 16:32:20 +0530

    update : removed unused variables

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,12 +1,7 @@
-import { RootState } from "@/redux/store";
 import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
 
 export const useAuth = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const userLoggedIn = useSelector(
-    (store: RootState) => store.user.userLoggedIn
-  );
 
   useEffect(() => {
     const token = localStorage.getItem("token");
