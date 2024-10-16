38829f8dc5612eca6de54012d4a70b89fb6bf0a0
AnkurDutta21 <daxxz2212@gmail.com>
2024-10-16 04:14:41 -0700

    api intregration hiring page

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,13 +3,16 @@ import comicsReducer from "./comicSlice";
 import userReducer from "./userSlice";
 import workshopReducer from "./workshopSlice";
 import authReducer from "./loginSlice";
-
+import careerReducer from "./careerSlice";
+import fileUploadReducer from "./fileUploadSlice";
 export const store = configureStore({
   reducer: {
     comics: comicsReducer,
     user: userReducer,
     workshop: workshopReducer,
     auth: authReducer,
+    career: careerReducer,
+    fileUpload: fileUploadReducer,
   },
 });
 
