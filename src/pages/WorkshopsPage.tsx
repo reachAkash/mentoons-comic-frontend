642830ec791040af219a08eca71cae22524c163b
reachAkash <akashgupta9062@gmail.com>
2024-10-17 16:39:04 +0530

    update : workshop page responsive

--- src/pages/WorkshopsPage.tsx
+++ src/pages/WorkshopsPage.tsx
@@ -252,7 +252,15 @@ export interface WorkshopItems {
 // export default ComicWorkshop;
 
 const WorkshopsPage = () => {
-  const [activeCategory, setActiveCategory] = React.useState<string>("6-12");
+  enum WorkshopType {
+    buddyCamp = "6-12",
+    teenCamp = "13-19",
+    "20+Camp" = "20+",
+    familyCamp = "Parents",
+  }
+  const [activeCategory, setActiveCategory] = React.useState<string>(
+    WorkshopType.buddyCamp
+  );
   const [showForm, setShowForm] = React.useState<boolean>(false);
 
   const handleCategoryChange = (category: string) => {
@@ -268,7 +276,7 @@ const WorkshopsPage = () => {
           className="w-full object-cover"
         /> */}
         <div
-          className={`flex items-center justify-center pt-12  ${
+          className={`flex items-center justify-center space-x-2 pt-12  ${
             activeCategory === "6-12"
               ? "bg-[#ffe5c8]"
               : activeCategory === "13-19"
@@ -277,8 +285,8 @@ const WorkshopsPage = () => {
           }`}
         >
           <div
-            className=" w-[10%]"
-            onClick={() => handleCategoryChange("6-12")}
+            className="w-[20%] md:w-[10%]"
+            onClick={() => handleCategoryChange(WorkshopType.buddyCamp)}
           >
             <img
               src={`/assets/images/${
@@ -291,8 +299,8 @@ const WorkshopsPage = () => {
             />
           </div>
           <div
-            className=" w-[10%]"
-            onClick={() => handleCategoryChange("13-19")}
+            className="w-[20%] md:w-[10%]"
+            onClick={() => handleCategoryChange(WorkshopType.teenCamp)}
           >
             <img
               src={`/assets/images/${
@@ -304,7 +312,10 @@ const WorkshopsPage = () => {
               className="w-36 object-cover"
             />
           </div>
-          <div className="w-[14%]" onClick={() => handleCategoryChange("20+")}>
+          <div
+            className="w-[27%] md:w-[14%]"
+            onClick={() => handleCategoryChange(WorkshopType["20+Camp"])}
+          >
             <img
               src={`/assets/images/${
                 activeCategory === "20+"
@@ -316,12 +327,12 @@ const WorkshopsPage = () => {
             />
           </div>
           <div
-            className=" w-[10%]"
-            onClick={() => handleCategoryChange("parents")}
+            className=" w-[20%] md:w-[10%]"
+            onClick={() => handleCategoryChange(WorkshopType.familyCamp)}
           >
             <img
               src={`/assets/images/${
-                activeCategory === "parents"
+                activeCategory === "Parents"
                   ? "parents-active-purple.png"
                   : "parents-purple.png"
               }`}
@@ -333,7 +344,7 @@ const WorkshopsPage = () => {
         <div>
           {activeCategory === "6-12" && <WorkshopsPage1 />}
           {activeCategory === "13-19" && <WorkshopsPage2 />}
-          {activeCategory === "parents" && <WorkshopsPage3 />}
+          {activeCategory === "Parents" && <WorkshopsPage3 />}
         </div>
         <div className="relative flex items-center justify-center left-0 px-8 py-3 cursor-pointer hover:scale-105 transition-all duration-300 ease-in-out">
           <img
@@ -347,7 +358,10 @@ const WorkshopsPage = () => {
             <DialogContent className="z-[999999] bg-transparent border-0">
               <DialogDescription>
                 <div className="relative">
-                  <WorkshopForm selectedWorkshop={activeCategory} setShowForm={setShowForm}/>
+                  <WorkshopForm
+                    selectedWorkshop={activeCategory}
+                    setShowForm={setShowForm}
+                  />
                 </div>
               </DialogDescription>
             </DialogContent>
@@ -362,17 +376,28 @@ export default WorkshopsPage;
 
 export const WorkshopsPage1 = () => {
   return (
-    <section>
-      <div className="w-full relative ">
+    <section className="bg-gray-200 md:bg-white">
+      <div className="w-full relative">
         <img
           src="/assets/images/buddy-camp-cloud.png"
           alt=""
-          className="w-full object-cover"
+          className="hidden md:block w-full object-cover"
         />
-
-        <div className=" absolute w-full h-full top-0 ">
-          <div className="flex items-start  justify-between   ">
-            <div className=" w-[50%] ">
+        <img
+          src="/assets/images/buddy_camp_cloud_sm.svg"
+          alt=""
+          className="block md:hidden w-full"
+        />
+        <div className="absolute w-full h-full top-0">
+          <div className="flex items-center justify-center pt-4">
+            <img
+              src="/Buddy camp.png"
+              alt=""
+              className="block md:hidden w-[85%] object-cover"
+            />
+          </div>
+          <div className="flex flex-col md:flex-row items-start justify-center md:justify-between">
+            <div className="md:w-[50%] flex flex-col items-center md:items-start">
               <img
                 src="/assets/images/workshop-hero-text.png"
                 alt=""
@@ -381,50 +406,79 @@ export const WorkshopsPage1 = () => {
               <img
                 src="/assets/images/workshop-subtext.png"
                 alt=""
-                className="w-[80%]  px-4  md:pt-0 md:px-12  object-cover"
+                className="hidden md:block w-[80%]  px-4  md:pt-0 md:px-12  object-cover"
               />
             </div>
-            <div className=" w-[50%] ">
+            <div className="md:w-[50%]">
               <img
                 src="/assets/images/workshop-hero.png"
                 alt=""
                 className="w-[90%] mx-auto object-cover "
               />
+              <img
+                src="/assets/images/workshop-subtext.png"
+                alt=""
+                className="block md:hidden w-[80%]  px-4  md:pt-0 md:px-12  object-cover"
+              />
             </div>
           </div>
         </div>
       </div>
-      <div className="relative  mt-12 ">
-        <img src="/assets/images/workshop-info-bg.png" alt="" />
-        <div className=" absolute top-0  w-full h-full flex ">
-          <div className="w-[50%] h-full ">
+      <div className="relative mt-12">
+        <img
+          className="hidden md:block"
+          src="/assets/images/workshop-info-bg.png"
+          alt=""
+        />
+        <img
+          className="block md:hidden"
+          src="/assets/images/workshop-info-bg-sm.svg"
+          alt=""
+        />
+        <div className="absolute top-0  w-full h-full flex flex-col md:flex-row">
+          <div className="md:w-[50%] h-full">
             <div>
               <img
                 src="/assets/images/issue-text.png"
                 alt=""
-                className="w-full object-cover p-4  md:p-12 "
+                className="hidden md:block w-full object-cover p-4  md:p-12"
+              />
+              <img
+                src="/assets/images/issue-text-sm.svg"
+                alt=""
+                className="block md:hidden w-full p-8"
               />
             </div>
             <div>
               <img
                 src="/assets/images/issue-illustration.png"
                 alt=""
-                className="w-full object-cover p-4 pt-0 md:p-12 md:pt-0 "
+                className="hidden md:block w-full object-cover p-4 pt-0 md:p-12 md:pt-0 "
+              />
+              <img
+                src="/assets/images/issue-illustration-sm.svg"
+                alt=""
+                className="absolute bottom-0 block md:hidden w-full object-cover p-4 pt-0 md:p-12 md:pt-0 "
               />
             </div>
           </div>
-          <div className="w-[50%] h-full  relative">
+          <div className="w-full md:w-[50%] h-full">
             <img
               src="/assets/images/mentoons-tv.png"
               alt=""
-              className="w-full object-cover  p-2 pt-4  pr-3 md:p-12"
+              className="block md:hidden absolute top-12 w-full object-cover p-2 pt-4 pr-3 md:p-12"
+            />
+            <img
+              src="/assets/images/mentoons-tv.png"
+              alt=""
+              className="hidden md:block w-full object-cover p-2 pt-4 pr-3 md:p-12"
             />
             <div>
               <video
                 playsInline
                 webkit-playsinline
                 src="https://mentoons-comics.s3.ap-northeast-1.amazonaws.com/AGES+6+-+12/Children+Common+Issues.mp4"
-                className=" w-[47%] absolute top-[33%] left-[25%]  md:top-[31%] md:left-[26%]"
+                className="w-[50%] md:w-[25%] absolute top-[20%] right-[26%] md:top-[31%] md:right-[12.6%]"
                 autoPlay
                 controls
                 muted
@@ -433,9 +487,9 @@ export const WorkshopsPage1 = () => {
           </div>
         </div>
       </div>
-      <div className="relative  flex">
-        <div className="w-[50%] h-full ">
-          <div>
+      <div className="hidden md:flex relative">
+        <div className="w-full md:w-[50%] h-full">
+          <div className="w-full">
             <img
               src="/assets/images/buddy-camp-info.png"
               alt=""
@@ -451,12 +505,43 @@ export const WorkshopsPage1 = () => {
           />
         </div>
       </div>
-      <div className="">
+      <div className="relative">
         <img
-          src="/assets/images/workshop-enquiry-form.png"
+          src="/assets/images/buddy-camp-info-sm.svg"
           alt=""
-          className="w-[80%] mx-auto"
+          className="w-full object-cover"
         />
+      </div>
+      <div className="relative">
+        <div>
+          <img
+            src="/assets/images/buddy-camp-creamCover-sm.svg"
+            alt=""
+            className="w-full object-cover"
+          />
+        </div>
+        <div className="absolute top-8">
+          <img
+            src="/assets/images/buddy-camp-info2-sm.svg"
+            alt=""
+            className="w-full object-cover p-4"
+          />
+        </div>
+        <div className="absolute bottom-0 flex items-center justify-center">
+          <img
+            src="/assets/images/issue-illustration-sm.svg"
+            alt=""
+            className="w-[80%]"
+          />
+        </div>
+      </div>
+      <div>
+        <div>
+          <img src="/assets/images/buddy-camp-childParent-header.svg" alt="" />
+        </div>
+        <div>
+          <img src="/assets/images/buddy-camp-childParent-sm.svg" alt="" />
+        </div>
       </div>
     </section>
   );
